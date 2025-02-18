b34a28fdc7c89a960dc216b348d6f8c1077429dd
kaustubhraut2001 <kaustubhr2001@gmail.com>
2025-02-18 19:04:19 +0530

    changed the count

--- Controllers/User.js
+++ Controllers/User.js
@@ -1245,12 +1245,12 @@ const get_report_by_id = async(req, res) => {
         const user = await User.findOne({ _id: id });
 
         if (user) {
-            if (user.submittedAssignmentCount === 539) {
+            if (user.submittedAssignmentCount === 529) {
                 // Check if incorrectAssignment and correctAssignment are already set
                 if (!user.incorrectAssignmentCount || !user.correctAssignmentCount) {
                     console.log(generateRandomNumber(), "####");
                     const correct = user.correctAssignmentCount = generateRandomNumber();
-                    user.incorrectAssignmentCount = 539 - correct;
+                    user.incorrectAssignmentCount = 529 - correct;
                     user.save();
                     res.status(200).json({ message: 'User Report...', user });
                 } else {
